6d4494d80e6317a2a6efd7caa970189729f66db5
Imran-2022 <mdimranulhaque202@gmail.com>
2024-04-30 21:39:57 +0600

    showing name instead of id of class in serializer

--- tuition/admin.py
+++ tuition/admin.py
@@ -4,4 +4,7 @@ from .models import TuitionPost
 class TuitionModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'duration', 'class_of_student', 'days_per_week', 'required_qualification', 'description']
 
+    # def abc(self, obj):
+    #     return obj.class_of_student.name
+
 admin.site.register(TuitionPost, TuitionModelAdmin)
--- tuition/serializers.py
+++ tuition/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from . import models
 
 class TuitionSerializer(serializers.ModelSerializer):
+    class_of_student=serializers.StringRelatedField(many=False)
+
     class Meta:
         model = models.TuitionPost
         fields = '__all__'
