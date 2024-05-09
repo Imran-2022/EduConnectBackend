23f6c63f66de7b1c71aabfaa80dc5e537b3f4cc3
Imran-2022 <mdimranulhaque202@gmail.com>
2024-05-09 18:00:10 +0600

    add creation time of a tuition post

--- tuition/models.py
+++ tuition/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from filter.models import Filter
+from django.utils import timezone
 
 class TuitionPost(models.Model):
     DURATION_CHOICES = [
@@ -36,6 +37,11 @@ class TuitionPost(models.Model):
     days_per_week = models.IntegerField(choices=DAYS_PER_WEEK_CHOICES)
     required_qualification = models.CharField(max_length=20, choices=QUALIFICATION_CHOICES)
     description = models.TextField()
+    creation_time = models.DateTimeField(default=timezone.now)  # Add creation time field
+
+    
+    def formatted_creation_time(self):
+        return self.creation_time.strftime('%Y-%m-%d')
 
     def __str__(self):
         return f"{self.class_of_student.name} Tuition Post ({self.duration}, {self.days_per_week} days/week)"
